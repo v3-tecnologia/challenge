040bf7574384a5e03c937c0c55ff7aa99b2eab83
mkafonso <mkafonso.dev@gmail.com>
2025-05-05 16:59:49 -0300

    test: add Gyroscope data saving usecase

---
+++ __tests__/usecase/save_gyroscope_data_usecase_test.go
@@ -0,0 +1,48 @@
+package usecase_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	repository "github.com/mkafonso/go-cloud-challenge/repository/memory"
+	"github.com/mkafonso/go-cloud-challenge/usecase"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSaveGyroscopeData_ShouldSaveSuccessfully(t *testing.T) {
+	repo := repository.NewInMemoryGyroscopeRepository()
+	uc := usecase.NewSaveGyroscopeData(repo)
+
+	request := &usecase.SaveGyroscopeDataRequest{
+		DeviceID:  "00:11:22:33:44:55",
+		X:         1.0,
+		Y:         2.0,
+		Z:         3.0,
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
+
+	response, err := uc.Execute(context.Background(), request)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+}
+
+func TestSaveGyroscopeData_ShouldFailWithInvalidTimestamp(t *testing.T) {
+	repo := repository.NewInMemoryGyroscopeRepository()
+	uc := usecase.NewSaveGyroscopeData(repo)
+
+	request := &usecase.SaveGyroscopeDataRequest{
+		DeviceID:  "00:11:22:33:44:55",
+		X:         1.0,
+		Y:         2.0,
+		Z:         3.0,
+		Timestamp: "invalid-timestamp",
+	}
+
+	response, err := uc.Execute(context.Background(), request)
+
+	assert.Error(t, err)
+	assert.Nil(t, response)
+	assert.EqualError(t, err, "invalid timestamp format")
+}
