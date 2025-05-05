79c015d2dab3e2bfb7e2ad6d3c93119d79f86306
mkafonso <mkafonso.dev@gmail.com>
2025-05-05 19:02:00 -0300

    feat: implement PostgreSQL repositories

---
+++ repository/postgres/postgres_gps_repository.go
@@ -0,0 +1,35 @@
+package postgres
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+	db "github.com/mkafonso/go-cloud-challenge/database/sqlc"
+	"github.com/mkafonso/go-cloud-challenge/entity"
+)
+
+type PostgresGPSRepository struct {
+	*db.Queries
+	connectionPool *pgxpool.Pool
+}
+
+func NewPostgresGPSRepository(pool *pgxpool.Pool) *PostgresGPSRepository {
+	return &PostgresGPSRepository{
+		connectionPool: pool,
+		Queries:        db.New(pool),
+	}
+}
+
+func (r *PostgresGPSRepository) SaveGPS(ctx context.Context, gps *entity.GPS) error {
+	params := db.SaveGPSParams{
+		ID:        gps.ID,
+		DeviceID:  gps.DeviceID,
+		Latitude:  gps.Latitude,
+		Longitude: gps.Longitude,
+		Timestamp: pgtype.Timestamptz{Time: gps.Timestamp, Valid: true},
+		CreatedAt: pgtype.Timestamptz{Time: gps.CreatedAt, Valid: true},
+	}
+
+	return r.Queries.SaveGPS(ctx, params)
+}
---
+++ repository/postgres/postgres_gyroscope_repository.go
@@ -0,0 +1,36 @@
+package postgres
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/jackc/pgx/v5/pgxpool"
+	db "github.com/mkafonso/go-cloud-challenge/database/sqlc"
+	"github.com/mkafonso/go-cloud-challenge/entity"
+)
+
+type PostgresGyroscopeRepository struct {
+	*db.Queries
+	connectionPool *pgxpool.Pool
+}
+
+func NewPostgresGyroscopeRepository(pool *pgxpool.Pool) *PostgresGyroscopeRepository {
+	return &PostgresGyroscopeRepository{
+		connectionPool: pool,
+		Queries:        db.New(pool),
+	}
+}
+
+func (r *PostgresGyroscopeRepository) SaveGyroscope(ctx context.Context, gyro *entity.Gyroscope) error {
+	params := db.SaveGyroscopeParams{
+		ID:        gyro.ID,
+		DeviceID:  gyro.DeviceID,
+		X:         gyro.X,
+		Y:         gyro.Y,
+		Z:         gyro.Z,
+		Timestamp: pgtype.Timestamptz{Time: gyro.Timestamp, Valid: true},
+		CreatedAt: pgtype.Timestamptz{Time: gyro.CreatedAt, Valid: true},
+	}
+
+	return r.Queries.SaveGyroscope(ctx, params)
+}
