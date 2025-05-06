19836fbf50dc8e7f9f268bf9c9063fce2e6648c2
ricardo <ricardorbxx1@gmail.com>
2025-05-06 10:47:56 -0300

    feat: setup logger and cors

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -4,9 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func (s *FiberServer) RegisterRoutes() {
+	s.Use(logger.New())
+	s.Use(cors.New())
+
 	s.App.Get("/health", func(c *fiber.Ctx) error {
 		return c.Status(http.StatusOK).JSON(fiber.Map{"status": "ok"})
 	})
