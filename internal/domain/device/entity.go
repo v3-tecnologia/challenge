cffef76a9cc661b636597072d79ca8020748f454
iamrosada0 <luisrosada@outlook.com>
2025-05-08 13:06:59 +0100

    feat: add NewDevice function to generate a device with MAC address validation

--- internal/domain/device/entity.go
+++ internal/domain/device/entity.go
@@ -1,5 +1,21 @@
 package device
 
+import (
+	"errors"
+	"regexp"
+)
+
 type Device struct {
 	ID string
+}
+
+func NewDevice(id string) (*Device, error) {
+	if !isValidMAC(id) {
+		return nil, errors.New("invalid MAC address")
+	}
+	return &Device{ID: id}, nil
+}
+
+func isValidMAC(id string) bool {
+	return regexp.MustCompile(`^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$`).MatchString(id)
 }
