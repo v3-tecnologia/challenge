e1c0a0dfc94ce321d0990f14da7c5b2876a66d66
iamrosada0 <luisrosada@outlook.com>
2025-05-08 19:01:56 +0100

    test: add test for successful creation in GyroscopeService

--- internal/tests/service/gyroscope_test.go
+++ internal/tests/service/gyroscope_test.go
@@ -1,8 +1,14 @@
 package service
 
 import (
-	d "github.com/iamrosada0/v3/internal/domain/gyroscope"
+	"testing"
+	"time"
 
+	"github.com/brianvoe/gofakeit"
+	d "github.com/iamrosada0/v3/internal/domain/gyroscope"
+	"github.com/iamrosada0/v3/internal/service/gyroscope"
+
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -13,4 +19,22 @@ type MockGyroscopeRepository struct {
 func (m *MockGyroscopeRepository) Create(data *d.GyroscopeData) (*d.GyroscopeData, error) {
 	args := m.Called(data)
 	return args.Get(0).(*d.GyroscopeData), args.Error(1)
+}
+func TestGyroscopeService_Create_Success(t *testing.T) {
+	mockRepo := new(MockGyroscopeRepository)
+	service := gyroscope.NewGyroscopeService(mockRepo)
+
+	deviceID := "00:1A:2B:3C:4D:5E"
+	x := gofakeit.Float64()
+	y := gofakeit.Float64()
+	z := gofakeit.Float64()
+	timestamp := time.Now()
+
+	data, _ := d.NewGyroscopeData(deviceID, x, y, z, timestamp)
+	mockRepo.On("Create", data).Return(nil)
+
+	result, err := service.Create(data)
+	assert.NoError(t, err)
+	assert.Equal(t, data, result)
+	mockRepo.AssertExpectations(t)
 }
