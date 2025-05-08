5022ed59f91f6f14e6bccef7bb708ef503bd841f
iamrosada0 <luisrosada@outlook.com>
2025-05-08 14:11:08 +0100

    test: add initial test file for gyroscope data

---
+++ internal/tests/domain/gyroscope_test.go
@@ -0,0 +1,27 @@
+package gyroscope
+
+import (
+	"testing"
+	"time"
+
+	"github.com/iamrosada0/v3/internal/domain/gyroscope"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewGyroscopeData_Success(t *testing.T) {
+	deviceID := "00:1A:2B:3C:4D:5E"
+	x := gofakeit.Float64()
+	y := gofakeit.Float64()
+	z := gofakeit.Float64()
+	timestamp := time.Now()
+
+	data, err := gyroscope.NewGyroscopeData(deviceID, x, y, z, timestamp)
+	assert.NoError(t, err)
+	assert.Equal(t, deviceID, data.Device.ID)
+	assert.Equal(t, x, data.X)
+	assert.Equal(t, y, data.Y)
+	assert.Equal(t, z, data.Z)
+	assert.Equal(t, timestamp, data.Timestamp)
+}
