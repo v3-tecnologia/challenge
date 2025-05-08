50b7a199480c672170b6d15d8c16e3c87cee10df
iamrosada0 <luisrosada@outlook.com>
2025-05-08 18:12:13 +0100

    test: replace mock service with repository mock in use case tests

--- internal/tests/usecase/gyroscope_test.go
+++ internal/tests/usecase/gyroscope_test.go
@@ -13,18 +13,18 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type MockGyroscopeService struct {
+type MockGyroscopeRepository struct {
 	mock.Mock
 }
 
-func (m *MockGyroscopeService) Create(data *d.GyroscopeData) error {
+func (m *MockGyroscopeRepository) Create(data *d.GyroscopeData) (*d.GyroscopeData, error) {
 	args := m.Called(data)
-	return args.Error(0)
+	return args.Get(0).(*d.GyroscopeData), args.Error(1)
 }
 
 func TestGyroscopeUseCase_Create_Success(t *testing.T) {
-	mockService := new(MockGyroscopeService)
-	uc := uc.NewGyroscopeUseCase(mockService)
+	mockRepo := new(MockGyroscopeRepository)
+	uc := uc.NewGyroscopeUseCase(mockRepo)
 
 	deviceID := "00:1A:2B:3C:4D:5E"
 	x := gofakeit.Float64()
@@ -33,11 +33,12 @@ func TestGyroscopeUseCase_Create_Success(t *testing.T) {
 	timestamp := time.Now()
 
 	data, _ := d.NewGyroscopeData(deviceID, x, y, z, timestamp)
-	mockService.On("Create", data).Return(nil)
+	mockRepo.On("Create", data).Return(data, nil)
 
-	_, err := uc.Create(deviceID, x, y, z, timestamp)
+	result, err := uc.Create(deviceID, x, y, z, timestamp)
 	assert.NoError(t, err)
-	mockService.AssertExpectations(t)
+	assert.Equal(t, data, result)
+	mockRepo.AssertExpectations(t)
 }
 
 func TestGyroscopeUseCase_Create_InvalidDeviceID(t *testing.T) {
