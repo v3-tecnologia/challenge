b7aca838c495fbafae17f82f2876b269fa502cc7
iamrosada0 <luisrosada@outlook.com>
2025-05-08 13:56:15 +0100

    feat: define GyroscopeRepository interface with Create method

--- internal/repository/gyroscope/repository.go
+++ internal/repository/gyroscope/repository.go
@@ -1,1 +1,9 @@
 package gyroscope
+
+import (
+	domain "github.com/iamrosada0/v3/internal/domain/gyroscope"
+)
+
+type GyroscopeRepository interface {
+	Create(d *domain.GyroscopeData) (*domain.GyroscopeData, error)
+}
