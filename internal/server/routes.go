a8d702faa0c65fc8177ca7529b559bf1f7f8e909
ricardo <ricardorbxx1@gmail.com>
2025-05-08 20:46:30 -0300

    feat: add handler for gps readings

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/ricardoraposo/challenge/internal/dto"
 	"github.com/ricardoraposo/challenge/internal/handlers"
 	"github.com/ricardoraposo/challenge/internal/middleware"
 	"github.com/ricardoraposo/challenge/internal/usecases"
@@ -18,11 +19,15 @@ func (s *FiberServer) RegisterRoutes() {
 	s.App.Get("/health", health)
 
 	gyroscopeUseCase := usecases.NewGyroscopeUseCase(s.Database.Query)
+	gpsUseCase := usecases.NewGPSUseCase(s.Database.Query)
+
 	gyroscopeHandler := handlers.NewGyroscopeHandler(gyroscopeUseCase)
+	gpsHandler := handlers.NewGPSHandler(gpsUseCase)
 
 	telemetryApi := s.App.Group("/telemetry")
 
-	telemetryApi.Post("/gyroscope", middleware.ValidateInsertGyroscopeReadingParams, gyroscopeHandler.CreateGyroscopeReadings)
+	telemetryApi.Post("/gyroscope", middleware.ValidateStruct[dto.InsertGryoscopeReadingsDto], gyroscopeHandler.CreateGyroscopeReadings)
+	telemetryApi.Post("/gps", middleware.ValidateStruct[dto.InsertGPSReadingsDto], gpsHandler.CreateGPSReadings)
 }
 
 func health(c *fiber.Ctx) error {
