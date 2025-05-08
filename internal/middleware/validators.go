05e69bb4bb22ce2e59bb42f94996b336110c684e
ricardo <ricardorbxx1@gmail.com>
2025-05-08 20:46:47 -0300

    feat: add middleware for validating structs

--- internal/middleware/validators.go
+++ internal/middleware/validators.go
@@ -3,22 +3,24 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/ricardoraposo/challenge/internal/common"
-	"github.com/ricardoraposo/challenge/internal/dto"
 )
 
-func ValidateInsertGyroscopeReadingParams(c *fiber.Ctx) error {
-	params := dto.InsertGryoscopeReadingsDto{}
+func ValidateStruct[T any](c *fiber.Ctx) error {
+	var params T
+
 	if err := c.BodyParser(&params); err != nil {
 		return err
 	}
 
 	validationErrors := common.Validate(params)
-
 	if len(validationErrors) > 0 {
 		return c.
 			Status(fiber.StatusBadRequest).
 			JSON(fiber.Map{"errors": validationErrors})
 	}
+
+	// Store the parsed and validated data in Locals to be used in the handler
+	c.Locals("validatedBody", params)
 
 	return c.Next()
 }
