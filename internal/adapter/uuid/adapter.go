d62877d6d2e0c9feeba4b09a36a1034eff4f5e01
iamrosada0 <luisrosada@outlook.com>
2025-05-08 13:25:49 +0100

    feat: define AdapterInterface for UUID generation

--- internal/adapter/uuid/adapter.go
+++ internal/adapter/uuid/adapter.go
@@ -3,6 +3,9 @@ package uuid
 import "github.com/google/uuid"
 
 type Adapter struct{}
+type AdapterInterface interface {
+	Value() string
+}
 
 func (u *Adapter) Value() string {
 	return uuid.New().String()
