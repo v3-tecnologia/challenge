42b4687d30b0af1d67267423a6c2326232aa95bb
iamrosada0 <luisrosada@outlook.com>
2025-05-11 09:02:08 +0100

    refactor: created a new structure of device

---
+++ internal/domain/device.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+	"errors"
+	"regexp"
+)
+
+var (
+	ErrInValidMACGyroscope = errors.New("invalid MAC address")
+)
+
+type Device struct {
+	ID string
+}
+
+func NewDevice(id string) (*Device, error) {
+	if !isValidMAC(id) {
+		return nil, ErrInValidMACGyroscope
+	}
+	return &Device{ID: id}, nil
+}
+
+func isValidMAC(id string) bool {
+	return regexp.MustCompile(`^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$`).MatchString(id)
+}
