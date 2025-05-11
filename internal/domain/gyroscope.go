1e1afe6658f546ddae1ed115185452f8dd0fb3a6
iamrosada0 <luisrosada@outlook.com>
2025-05-11 09:15:11 +0100

    feat: created the DTO and model of Gyroscope

--- internal/domain/gyroscope.go
+++ internal/domain/gyroscope.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"errors"
+	"time"
 )
 
 var (
@@ -9,3 +10,21 @@ var (
 	ErrTimestampGyroscope     = errors.New("timestamp not found")
 	ErrInvalidGyroscopeValues = errors.New("invalid gyroscope values")
 )
+
+type GyroscopeDto struct {
+	DeviceID  string  `json:"deviceId" binding:"required"`
+	Timestamp int64   `json:"timestamp" binding:"required"`
+	X         float64 `json:"x" binding:"required"`
+	Y         float64 `json:"y" binding:"required"`
+	Z         float64 `json:"z" binding:"required"`
+}
+
+type Gyroscope struct {
+	ID        string    `json:"id" gorm:"primaryKey"`
+	DeviceID  string    `json:"device_id" gorm:"index;not null"`
+	X         float64   `json:"x" gorm:"not null"`
+	Y         float64   `json:"y" gorm:"not null"`
+	Z         float64   `json:"z" gorm:"not null"`
+	Timestamp time.Time `json:"timestamp" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+}
