2cc7355deb38a44c5a5feb183d8b40482c91d0a7
iamrosada0 <luisrosada@outlook.com>
2025-05-11 23:19:30 +0100

    feat:  add auto migrate to generate table

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"v3/internal/domain"
 	"v3/internal/infra/api"
 	"v3/internal/repository/gps"
 	"v3/internal/repository/gyroscope"
@@ -14,37 +15,30 @@ import (
 )
 
 func main() {
-	// Initialize database
 	dsn := "host=postgres user=meuusuario password=minhasenha dbname=meubanco port=5432 sslmode=disable"
 
-	// Conectar ao banco de dados
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to the database: %v", err)
 	}
 	fmt.Println("Conectado ao banco de dados com sucesso!")
 
-	// Initialize repositories
+	//  AutoMigrate para criar a tabela gyroscopes
+	err = db.AutoMigrate(&domain.Gyroscope{}, &domain.GPS{})
+	if err != nil {
+		log.Fatalf("erro ao fazer AutoMigrate: %v", err)
+	}
+
+	// Inicialização dos repositórios e casos de uso
 	gyroRepo := gyroscope.NewGyroscopeRepository(db)
 	gpsRepo := gps.NewGPSRepository(db)
-	// photoRepo := photo.NewPhotoRepository(db)
 
-	// Initialize AWS Rekognition client (simplified)
-	//rekogClient := initializeRekognitionClient() // Custom function
-
-	// Initialize use cases
 	createGyroUseCase := usecase.NewCreateGyroscopeUseCase(gyroRepo)
 	createGPSUseCase := usecase.NewCreateGPSUseCase(gpsRepo)
-	// createPhotoUseCase := usecase.NewCreatePhotoUseCase(photoRepo, rekogClient)
 
-	// Initialize handlers
 	gyroHandlers := api.NewGyroscopeHandlers(createGyroUseCase)
 	gpsHandlers := api.NewGPSHandlers(createGPSUseCase)
-	// photoHandlers := api.NewPhotoHandlers(createPhotoUseCase)
 
-	// Setup router
 	router := api.SetupRouter(gyroHandlers, gpsHandlers)
-
-	// Start server
 	router.Run(":8080")
 }
