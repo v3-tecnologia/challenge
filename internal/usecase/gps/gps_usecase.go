c6151e1425d943f3942771ba7f82f43c9e13b57d
iamrosada0 <luisrosada@outlook.com>
2025-05-11 11:10:52 +0100

    feat: added CreateGPSUseCase handles the creation of a GPS entity

--- internal/usecase/gps/gps_usecase.go
+++ internal/usecase/gps/gps_usecase.go
@@ -1,1 +1,14 @@
 package gps
+
+import "github.com/iamrosada0/v3/internal/repository/gps"
+
+type GPSInputDto struct {
+	DeviceID  string  `json:"deviceId"`
+	Timestamp int64   `json:"timestamp"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type CreateGPSUseCase struct {
+	Repo gps.GPSRepository
+}
