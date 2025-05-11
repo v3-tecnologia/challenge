d9f63dcb1b0a50cf3c51594700454077d3010a3b
iamrosada0 <luisrosada@outlook.com>
2025-05-11 10:36:50 +0100

    feat: add NewPhotoRepository creates a new instance of PhotoRepository

--- internal/repository/photo/photo_repository.go
+++ internal/repository/photo/photo_repository.go
@@ -1,7 +1,18 @@
 package photo
 
-import "github.com/iamrosada0/v3/internal/domain"
+import (
+	"github.com/iamrosada0/v3/internal/domain"
+	"gorm.io/gorm"
+)
 
 type PhotoRepository interface {
 	Create(d *domain.Photo) (*domain.Photo, error)
+}
+
+type photoRepository struct {
+	DB *gorm.DB
+}
+
+func NewPhotoRepository(db *gorm.DB) PhotoRepository {
+	return &photoRepository{DB: db}
 }
