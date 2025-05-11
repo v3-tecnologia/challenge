9d11e6d810d1b119416eca234193299082122ff6
iamrosada0 <luisrosada@outlook.com>
2025-05-11 10:33:31 +0100

    feat: gyroscopeRepository is the concrete implementation of GyroscopeRepository

--- internal/repository/gyroscope/gyroscope_repository.go
+++ internal/repository/gyroscope/gyroscope_repository.go
@@ -1,7 +1,13 @@
 package gyroscope
 
-import "github.com/iamrosada0/v3/internal/domain"
+import (
+	"github.com/iamrosada0/v3/internal/domain"
+	"gorm.io/gorm"
+)
 
 type GyroscopeRepository interface {
 	Create(d *domain.Gyroscope) (*domain.Gyroscope, error)
+}
+type gyroscopeRepository struct {
+	DB *gorm.DB
 }
