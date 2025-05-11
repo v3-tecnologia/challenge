efb4b253da5df9195629a9cdc323adc5a2826a06
iamrosada0 <luisrosada@outlook.com>
2025-05-11 11:28:36 +0100

    feat: Implementing   CreatePhotoUseCase handles the creation of a Photo entity with AWS Rekognition integration.

--- internal/usecase/photos/photo_usecase.go
+++ internal/usecase/photos/photo_usecase.go
@@ -1,1 +1,17 @@
 package photos
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/rekognition"
+	"github.com/iamrosada0/v3/internal/repository/photo"
+)
+
+type PhotoInputDto struct {
+	DeviceID  string `json:"deviceId"`
+	Timestamp int64  `json:"timestamp"`
+	FilePath  string `json:"file_path"`
+}
+
+type CreatePhotoUseCase struct {
+	Repo        photo.PhotoRepository
+	RekogClient *rekognition.Client
+}
