a3d488f6eca301ddb3bd049eba27fbb27086b45a
iamrosada0 <luisrosada@outlook.com>
2025-05-12 00:06:13 +0100

    feat: add photo handler

--- internal/infra/api/photo_handler.go
+++ internal/infra/api/photo_handler.go
@@ -1,30 +1,44 @@
 package api
 
-// type PhotoHandlers struct {
-// 	CreatePhotoUseCase *usecase.CreatePhotoUseCase
-// }
+import (
+	"errors"
+	"net/http"
+	"v3/internal/domain"
+	"v3/internal/usecase"
 
-// func NewPhotoHandlers(createPhotoUseCase *usecase.CreatePhotoUseCase) *PhotoHandlers {
-// 	return &PhotoHandlers{
-// 		CreatePhotoUseCase: createPhotoUseCase,
-// 	}
-// }
+	"github.com/gin-gonic/gin"
+)
 
-// func (h *PhotoHandlers) CreatePhotoHandler(c *gin.Context) {
-// 	var input domain.PhotoDto
-// 	if err := c.ShouldBindJSON(&input); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": domain.ErrMissingGPSInvalidFields})
-// 		return
-// 	}
+type PhotoHandlers struct {
+	CreatePhotoUseCase *usecase.CreatePhotoUseCase
+}
 
-// 	photo, err := h.CreatePhotoUseCase.Execute(input)
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
+func NewPhotoHandlers(createPhotoUseCase *usecase.CreatePhotoUseCase) *PhotoHandlers {
+	return &PhotoHandlers{
+		CreatePhotoUseCase: createPhotoUseCase,
+	}
+}
 
-// 	c.JSON(http.StatusCreated, gin.H{
-// 		"photo":      photo,
-// 		"recognized": photo.Recognized,
-// 	})
-// }
+func (h *PhotoHandlers) SetupRoutes(router *gin.Engine) {
+	router.POST("/api/telemetry/photo", h.CreatePhotoHandler)
+}
+
+func (h *PhotoHandlers) CreatePhotoHandler(c *gin.Context) {
+	var input domain.PhotoDto
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": domain.ErrMissingPhotoInvalidFields.Error()})
+		return
+	}
+
+	photo, err := h.CreatePhotoUseCase.Execute(input)
+	if err != nil {
+		if errors.Is(err, domain.ErrDeviceIDPhoto) || errors.Is(err, domain.ErrTimestampPhoto) || errors.Is(err, domain.ErrPhotoData) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+
+	c.JSON(http.StatusCreated, photo)
+}
