92415ace63f281044271b9b55be52c1bb17b03f2
iamrosada0 <luisrosada@outlook.com>
2025-05-11 12:04:37 +0100

    feat: SetupRoutes configures the API routes for Gyroscope endpoints.

--- internal/infra/api/gyroscope_handler.go
+++ internal/infra/api/gyroscope_handler.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/gin-gonic/gin"
 	usecase "github.com/iamrosada0/v3/internal/usecase/gyroscope"
 )
 
@@ -11,5 +12,12 @@ type GyroscopeHandlers struct {
 func NewGyroscopeHandlers(createGyroscopeUseCase *usecase.CreateGyroscopeUseCase) *GyroscopeHandlers {
 	return &GyroscopeHandlers{
 		CreateGyroscopeUseCase: createGyroscopeUseCase,
+	}
+}
+
+func (h *GyroscopeHandlers) SetupRoutes(router *gin.Engine) {
+	api := router.Group("/api/telemetry")
+	{
+		api.POST("/gyroscope", h.CreateGyroscopeHandler)
 	}
 }
