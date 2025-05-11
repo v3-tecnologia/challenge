2eff162dcfe933e0e6f94a3171b0f7484020d487
iamrosada0 <luisrosada@outlook.com>
2025-05-11 11:56:17 +0100

    feat: SetupRoutes configures the API routes for GPS endpoints.

--- internal/infra/api/gps_handler.go
+++ internal/infra/api/gps_handler.go
@@ -1,9 +1,23 @@
 package api
 
 import (
+	"github.com/gin-gonic/gin"
 	usecase "github.com/iamrosada0/v3/internal/usecase/gps"
 )
 
 type GPSHandlers struct {
 	CreateGPSUseCase *usecase.CreateGPSUseCase
+}
+
+func NewGPSHandlers(createGPSUseCase *usecase.CreateGPSUseCase) *GPSHandlers {
+	return &GPSHandlers{
+		CreateGPSUseCase: createGPSUseCase,
+	}
+}
+
+func (h *GPSHandlers) SetupRoutes(router *gin.Engine) {
+	api := router.Group("/api/telemetry")
+	{
+		api.POST("/gps", h.CreateGPSHandler)
+	}
 }
