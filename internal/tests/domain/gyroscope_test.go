6c0e22aa858c5d8cf4c1af5275a62143d4ffad20
iamrosada0 <luisrosada@outlook.com>
2025-05-12 13:34:54 +0100

    test: gyroscope test

--- internal/tests/domain/gyroscope_test.go
+++ internal/tests/domain/gyroscope_test.go
@@ -1,38 +1,96 @@
-package gyroscope
+package domain
 
-// import (
-// 	"testing"
-// 	"time"
+import (
+	"errors"
+	"math"
+	"testing"
+	"time"
+	"v3/internal/domain"
+)
 
-// 	"github.com/brianvoe/gofakeit/v6"
-// 	"github.com/stretchr/testify/assert"
-// )
+func TestNewGyroscopeData(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   *domain.GyroscopeDto
+		wantErr error
+	}{
+		{
+			name: "Valid gyroscope data",
+			input: &domain.GyroscopeDto{
+				DeviceID:  "00:0a:95:9d:68:16",
+				Timestamp: time.Now().Unix(),
+				X:         1.0,
+				Y:         2.0,
+				Z:         3.0,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Invalid Device ID",
+			input: &domain.GyroscopeDto{
+				DeviceID:  "invalid-mac",
+				Timestamp: time.Now().Unix(),
+				X:         1.0,
+				Y:         2.0,
+				Z:         3.0,
+			},
+			wantErr: domain.ErrDeviceIDGyroscope,
+		},
+		{
+			name: "Zero timestamp",
+			input: &domain.GyroscopeDto{
+				DeviceID:  "00:0a:95:9d:68:16",
+				Timestamp: 0,
+				X:         1.0,
+				Y:         2.0,
+				Z:         3.0,
+			},
+			wantErr: domain.ErrTimestampGyroscope,
+		},
+		{
+			name: "Invalid X (NaN)",
+			input: &domain.GyroscopeDto{
+				DeviceID:  "00:0a:95:9d:68:16",
+				Timestamp: time.Now().Unix(),
+				X:         math.NaN(),
+				Y:         2.0,
+				Z:         3.0,
+			},
+			wantErr: domain.ErrInvalidGyroscopeValues,
+		},
+		{
+			name: "Invalid Y (Inf)",
+			input: &domain.GyroscopeDto{
+				DeviceID:  "00:0a:95:9d:68:16",
+				Timestamp: time.Now().Unix(),
+				X:         1.0,
+				Y:         math.Inf(1),
+				Z:         3.0,
+			},
+			wantErr: domain.ErrInvalidGyroscopeValues,
+		},
+		{
+			name: "Invalid Z (NaN)",
+			input: &domain.GyroscopeDto{
+				DeviceID:  "00:0a:95:9d:68:16",
+				Timestamp: time.Now().Unix(),
+				X:         1.0,
+				Y:         2.0,
+				Z:         math.NaN(),
+			},
+			wantErr: domain.ErrInvalidGyroscopeValues,
+		},
+	}
 
-// func TestNewGyroscopeData_Success(t *testing.T) {
-// 	deviceID := "00:1A:2B:3C:4D:5E"
-// 	x := gofakeit.Float64()
-// 	y := gofakeit.Float64()
-// 	z := gofakeit.Float64()
-// 	timestamp := time.Now()
-
-// 	data, err := gyroscope.NewGyroscopeData(deviceID, x, y, z, timestamp)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, deviceID, data.Device.ID)
-// 	assert.Equal(t, x, data.X)
-// 	assert.Equal(t, y, data.Y)
-// 	assert.Equal(t, z, data.Z)
-// 	assert.Equal(t, timestamp, data.Timestamp)
-// }
-// func TestNewGyroscopeData_InvalidDeviceID(t *testing.T) {
-// 	data, err := gyroscope.NewGyroscopeData("invalid-mac", 1.0, 2.0, 3.0, time.Now())
-// 	assert.Error(t, err)
-// 	assert.Nil(t, data)
-// 	assert.Equal(t, "invalid MAC address", err.Error())
-// }
-
-// func TestNewGyroscopeData_InvalidTimestamp(t *testing.T) {
-// 	data, err := gyroscope.NewGyroscopeData("00:1A:2B:3C:4D:5E", 1.0, 2.0, 3.0, time.Time{})
-// 	assert.Error(t, err)
-// 	assert.Nil(t, data)
-// 	assert.Equal(t, "timestamp is required", err.Error())
-// }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := domain.NewGyroscopeData(tt.input)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("NewGyroscopeData() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err == nil && got == nil {
+				t.Error("Expected gyroscope data, got nil")
+			}
+		})
+	}
+}
