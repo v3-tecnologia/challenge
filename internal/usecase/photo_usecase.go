6ef5738163d9ce87ab2a10cdd997072af7e5d501
iamrosada0 <luisrosada@outlook.com>
2025-05-12 11:57:51 +0100

    feat: photo usecase

--- internal/usecase/photo_usecase.go
+++ internal/usecase/photo_usecase.go
@@ -1,56 +1,98 @@
 package usecase
 
-// import (
-// 	"context"
+import (
+	"fmt"
+	"v3/internal/domain"
+	"v3/internal/infra/aws"
+	"v3/internal/repository/photo"
+)
 
-// 	"github.com/aws/aws-sdk-go-v2/aws"
-// 	"github.com/aws/aws-sdk-go-v2/service/rekognition"
-// 	"github.com/aws/aws-sdk-go-v2/service/rekognition/types"
+type CreatePhotoUseCase struct {
+	Repo       photo.PhotoRepository
+	AWSService *aws.AWSService
+}
+
+func NewCreatePhotoUseCase(repo photo.PhotoRepository, awsService *aws.AWSService) *CreatePhotoUseCase {
+	return &CreatePhotoUseCase{
+		Repo:       repo,
+		AWSService: awsService,
+	}
+}
+
+func (uc *CreatePhotoUseCase) Execute(input domain.PhotoDto, photoBytes []byte) (*domain.Photo, error) {
+	photo, err := domain.NewPhotoData(&input)
+	if err != nil {
+		return nil, err
+	}
+	if len(photoBytes) == 0 {
+		return nil, domain.ErrPhotoData
+	}
+
+	photoPath, err := uc.AWSService.UploadPhoto(input.DeviceID, photoBytes, input.Timestamp)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", domain.ErrProcessPhotoWithAWSRekognition, err)
+	}
+	photo.FilePath = photoPath
+
+	recognized, err := uc.AWSService.ComparePhoto(input.DeviceID, photoPath)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", domain.ErrProcessPhotoWithAWSRekognition, err)
+	}
+	photo.Recognized = recognized
+
+	savedPhoto, err := uc.Repo.Create(photo)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %v", domain.ErrSavePhotoData, err)
+	}
+
+	return savedPhoto, nil
+}
+
+// package usecase
+
+// import (
+// 	"fmt"
 // 	"v3/internal/domain"
+// 	"v3/internal/infra/aws"
 // 	"v3/internal/repository/photo"
 // )
 
 // type CreatePhotoUseCase struct {
-// 	Repo        photo.PhotoRepository
-// 	RekogClient *rekognition.Client
+// 	Repo       photo.PhotoRepository
+// 	AWSService *aws.AWSService
 // }
 
-// func NewCreatePhotoUseCase(repo photo.PhotoRepository, rekogClient *rekognition.Client) *CreatePhotoUseCase {
+// func NewCreatePhotoUseCase(repo photo.PhotoRepository, awsService *aws.AWSService) *CreatePhotoUseCase {
 // 	return &CreatePhotoUseCase{
-// 		Repo:        repo,
-// 		RekogClient: rekogClient,
+// 		Repo:       repo,
+// 		AWSService: awsService,
 // 	}
 // }
 
 // func (uc *CreatePhotoUseCase) Execute(input domain.PhotoDto) (*domain.Photo, error) {
-
-// 	photo, err := domain.NewPhotoData(&domain.PhotoDto{
-// 		DeviceID:  input.DeviceID,
-// 		Timestamp: input.Timestamp,
-// 		FilePath:  input.FilePath,
-// 	})
+// 	photo, err := domain.NewPhotoData(&input)
 // 	if err != nil {
 // 		return nil, err
 // 	}
-// 	inputRekog := &rekognition.SearchFacesByImageInput{
-// 		CollectionId: aws.String("your-collection-id"),
-// 		Image: &types.Image{
-// 			S3Object: &types.S3Object{
-// 				Bucket: aws.String("your-bucket"),
-// 				Name:   aws.String(photo.FilePath),
-// 			},
-// 		},
-// 	}
-// 	result, err := uc.RekogClient.SearchFacesByImage(context.TODO(), inputRekog)
+
+// 	// Upload photo to S3
+// 	photoPath, err := uc.AWSService.UploadPhoto(input.DeviceID, input.Photo, input.Timestamp)
 // 	if err != nil {
-// 		return nil, domain.ErrProcessPhotoWithAWSRekognition
+// 		return nil, fmt.Errorf("%w: %v", domain.ErrProcessPhotoWithAWSRekognition, err)
 // 	}
+// 	photo.FilePath = photoPath
 
-// 	photo.Recognized = len(result.FaceMatches) > 0
+// 	// Compare with previous photos
+// 	recognized, err := uc.AWSService.ComparePhoto(input.DeviceID, photoPath)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("%w: %v", domain.ErrProcessPhotoWithAWSRekognition, err)
+// 	}
+// 	photo.Recognized = recognized
 
+// 	// Save to database
 // 	savedPhoto, err := uc.Repo.Create(photo)
 // 	if err != nil {
-// 		return nil, domain.ErrSavePhotoData
+// 		return nil, fmt.Errorf("%w: %v", domain.ErrSavePhotoData, err)
 // 	}
 
 // 	return savedPhoto, nil
