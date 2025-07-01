b238bf8639d3d29b1228ae127622c41235702be6
rodrigo dos santos <ogirdo.sant@gmail.com>
2025-06-30 21:52:46 -0300

    COnfiguração de hosts

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.228.189.63']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
